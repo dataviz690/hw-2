dbfd20df547570a5a846f0c25fc8b4336f11a523
Gianetti <lgianetti@Cambridgema.gov>
2026-02-26 07:51:38 -0500

    Update code_for_hw2_plots.R

--- code_for_hw2_plots.R
+++ code_for_hw2_plots.R
@@ -24,15 +24,23 @@ str(mydata)
 # plot 1 ----------------------------------------------------------
 library(ggplot2)
 
-base= ggplot(data=mydata) 
-plot1= base + 
-  aes(x=gear) + 
-  geom_bar() + 
+base = ggplot(data = mydata) 
+plot1 = base + 
+  aes(x = gear) + 
+  geom_bar(fill = "lightgrey") + 
+  geom_text(stat ='count', aes(label = after_stat(count)), colour = "blue", size = 10, hjust = 1) +
   coord_flip() +
-  theme(axis.title.x = element_blank(),
+  labs(
+    title = 'Count of Cars per GEAR',
+    caption = 'Source: mtcars data',
+    x ='Gear') +
+  theme_classic() +
+  theme(plot.title = element_text(colour = "lightgrey", face = "bold"),
+    axis.title.x = element_blank(),
           axis.line.x = element_blank(),
           axis.text.x = element_blank(),
-          axis.ticks.x = element_blank())
+          axis.ticks.x = element_blank(),
+          axis.title.y = element_text(face = "bold"))
 plot1
 
 
@@ -44,7 +52,22 @@ saveRDS(plot1, file = "plot1.rds")
 
 # plot 2 ----------------------------------------------------------
 
-plot2= base + aes(x=factor(carb))+ geom_bar()
+plot2 = base + 
+  aes(x = factor(carb)) + 
+  geom_bar(fill = "yellow") +
+  geom_text(stat='count', aes(label= after_stat(count)), colour = "darkgreen", vjust=-0.1) +
+  labs(
+    title = 'Count of Cars per CARB',
+    caption = 'Source: mtcars data',
+    x='CARB') +
+  theme_classic() +
+  theme(plot.title = element_text(colour = "black", face = "bold"),
+        axis.title.y = element_blank(),
+        axis.line.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.x = element_text(face = "bold"))
+
 plot2
 
 
@@ -54,7 +77,10 @@ saveRDS(plot2, file = "plot2.rds")
 
 # plot 3 ----------------------------------------------------------
 
-plot3= base + aes(x=cyl) + geom_bar() + facet_grid(gear~carb,labeller = label_both)
+plot3 = base + 
+  aes(x = cyl) + 
+  geom_bar(fill = "yellow") + 
+  facet_grid(gear~carb,labeller = label_both)
 plot3
 
 # save plot3 ----------------------------------------------------------
